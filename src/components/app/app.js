45d35b0b472b3d088c745d2eec794c596320b7e6
daukaaa <daruen@list.ru>
2023-12-05 01:08:05 +0600

    code reafactoring 2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -64,6 +64,7 @@ export default class App extends Component{
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllPlanets}
+                            renderItem={item => (<span>{item.name} <button>!</button></span>)}
                         />
                     </div>
                     <div className="col-md-6">
