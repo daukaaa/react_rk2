f00c2d06509a5214d4d34c0cb7a860e79d42df19
daukaaa <daruen@list.ru>
2023-12-05 01:25:10 +0600

    add row component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,11 +12,6 @@ import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import SwapiService from "../../services/swapi-service";
 
-
-
-
-
-
 export default class App extends Component{
     swapiService = new SwapiService();
 
@@ -64,7 +59,7 @@ export default class App extends Component{
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllPlanets}
-                            renderItem={item => (<span>{item.name} <button>!</button></span>)}
+                            renderItem={  (item) => <span>{item.name} <button>!</button></span>}
                         />
                     </div>
                     <div className="col-md-6">
@@ -78,7 +73,7 @@ export default class App extends Component{
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllStarships}
-                        />
+                            renderItem={  ({ name, gender, birthYear }) => `${name} 5`}                        />
                     </div>
                     <div className="col-md-6">
                         <PersonDetails personId={this.state.selectedPerson} />
